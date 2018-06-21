fe921b504ff04124a35839e64b8a72d95e706494
isabelavp <veliz_isabel@hotmail.com>
2018-06-21 11:55:25 -0400

    subiendo cambios en fetch

--- src/data.js
+++ src/data.js
@@ -1,9 +1,8 @@
-//variables conts y let
 const btn2 = document.getElementById('btn2');
 const btn3 = document.getElementById('progress');
 
 const container = document.getElementById('cohorts');
-const students = document.getElementById('texto');
+//const students = document.getElementById('texto');
 const usersJSON = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
 const progressJSON = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
 const cohortsJSON = '../data/cohorts.json';
@@ -13,7 +12,9 @@ let cohorts = null;
 
 
 
-//	creando fetch del user
+//  creando fetch del user
+
+/*
 fetch(usersJSON)
   .then(response => response.json())
   .then(data => {
@@ -25,54 +26,76 @@ fetch(usersJSON)
 
 
 const renderUsers = users => {
-	btn.addEventListener("click", () => {
-		const render = users.forEach(element =>{ 
-			// element.name ==
-			let myUsers = element.name;
-			let myListUsers = document.createElement("ul");
-			let listItemUsers = document.createElement("li");
-			let bItem = document.createElement("button");
-			bItem.textContent = myUsers;
-			bItem.addEventListener("click", () => {
-				students.innerHTML = `<div id="texto">
-				<p>${element.name}</p>
-				</div>`
-			})
-			myListUsers.appendChild(bItem);
-			students.appendChild(myListUsers);
-		
-		})
-	})
-}
+  btn.addEventListener("click", () => {
+    const render = users.forEach(element =>{ 
+      // element.name ==
+      let myUsers = element.name;
+      let myListUsers = document.createElement("ul");
+      let listItemUsers = document.createElement("li");
+      let bItem = document.createElement("button");
+      bItem.textContent = myUsers;
+      bItem.addEventListener("click", () => {
+        students.innerHTML = `<div id="texto">
+        <p>${element.name}</p>
+        </div>`
+      })
+      myListUsers.appendChild(bItem);
+      students.appendChild(myListUsers);
+    
+    })
+  })
+} */
+
+
+
 
 //creando fetch del cohort
 
 fetch(cohortsJSON)
   .then(response => response.json())
-  .then(data =>{
+  .then(data => {
     cohorts = data;
+
     console.log(data);
     renderCohorts(data);
   });
-  
-const renderCohorts = cohorts => {
-  btn2.addEventListener('click', () => {
-    const render = cohorts.forEach(element => {
-      // element.name ==
-      let myCohorts = element.id;
-      let myList = document.createElement('ul');
-      let listItem = document.createElement('li');
-      let aItem = document.createElement('button');
-      aItem.textContent = myCohorts;
-      aItem.addEventListener('click', () => {
-        students.innerHTML = `<p id='texto'>`+ renderUsers +`</p>`;
-      });
-      myList.appendChild(aItem);
-      container.appendChild(myList);
-    });
-  });
-};
 
+const renderCohorts = cohorts => {
+    btn2.addEventListener('click', () => {
+          const render = cohorts.forEach(element => {
+            // element.name ==
+            let myCohorts = element.id;
+            let myList = document.createElement('ul');
+            let listItem = document.createElement('li');
+            let aItem = document.createElement('button');
+            aItem.textContent = myCohorts;
+            myList.appendChild(aItem);
+            container.appendChild(myList);
+            aItem.addEventListener('click', users => {
+
+              fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json')
+                .then((usersResponse) => { // es una función
+                  return usersResponse.json(); // hace la conexión y lo definimos como queremos que nos devuelvan los datos
+                })
+                .then(function(data) { // otra función
+                  let html = '';
+                  data.forEach(function(users) {
+                    html +=
+                      `
+        <li>${users.name}</li>`;
+                    console.log(data);
+                  });
+                  document.getElementById('resultado').innerHTML = html;
+                })
+                .catch((error) => {
+                  console.log(error);
+                });
+
+
+            })
+          })
+        })
+  }
 /*
 
 window.computeUsersStats = (users, progress, courses) => {
