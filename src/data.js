60136cfdcc4371112321b2be03fecf456a664010
Carolina Lastra <carolastra.cofre@gmail.com>
2018-06-24 17:18:13 -0400

    Arreglos HTML, actualizaciones y arreglo identación

--- src/data.js
+++ src/data.js
@@ -1,125 +1,112 @@
 const btn2 = document.getElementById('btn2');
-//const myInput = document.getElementById('myInput');
+//  const myInput = document.getElementById('myInput');
 
-
-myInput.addEventListener('onkeyup',function() {
-	
-		
-		filterUsers();
-	
+myInput.addEventListener('onkeyup', function() {
+  filterUsers();
 });
 
-//constantes de los id donde va el cohort y las alumnas
+//  Constantes de los id donde va el cohort y las alumnas
 
 const container = document.getElementById('cohorts');
 const students = document.getElementById('texto')
 
-//variables de la data
+//  Variables de la data
 const usersJSON = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
 const progressJSON = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
 const cohortsJSON = '../data/cohorts.json';
 let users = null;
-//variables globales
+
+//  Variables globales
 let progress = null;
 let cohorts = null;
 let usersStats = null;
 
-//codigo de fetch para user, progress y cohort
-//fetch de la data de cohort
+//  Código de fetch para user, progress y cohort
+// Fetch de la data de cohort
 fetch(cohortsJSON)
   .then(response => response.json())
   .then(data => {
     cohorts = data;
-    
 
     console.log(data);
     renderCohorts(data);
-    if(users && progress && cohorts){
-    	
+    if (users && progress && cohorts) {
     }
   });
-  
-  //fetch de la data progreso
-  fetch(progressJSON)
+
+//  Fetch de la data progreso
+fetch(progressJSON)
   .then(response => response.json())
   .then(data => {
     progress = data;
-    
 
     console.log(data);
     renderCohorts(data);
-    if(users && progress && cohorts){
-    	
+    if (users && progress && cohorts) {
+
     }
   });
 
-  //fetch de la data de los usuarios
+//  Fetch de la data de los usuarios
 
-  fetch(usersJSON)
+fetch(usersJSON)
   .then(response => response.json())
   .then(data => {
     users = data;
-    
 
     console.log(data);
     renderCohorts(data);
-    if(users && progress && cohorts){
-    	return 
+    if (users && progress && cohorts) {
+      return
     }
   });
 
-//constante de declaracion de cohorts
-
+//  Constante de declaracion de cohorts
 
 const renderCohorts = cohorts => {
-    btn2.addEventListener('click', () => {
-          const render = cohorts.forEach(element => {
-            // element.name ==
-            let myCohorts = element.id;
-            let myList = document.createElement('ul');
-            let listItem = document.createElement('li');
-            let aItem = document.createElement('button');
-            aItem.textContent = myCohorts;
-            myList.appendChild(aItem);
-            container.appendChild(myList);
-            aItem.addEventListener('click', () => {
-            let usersStats = window.computeUsersStats(users, progress, Object.keys(element.coursesIndex));
-            let myListUP = document.createElement('ul');
-            let listItemUP = document.createElement('li');
-            let aItemUP = document.createElement('button');
-            aItemUP.textContent = usersStats.sort(); //uso de sort para tener nombres por orden alfabetico
-            myListUP.appendChild(aItemUP);
-            students.appendChild(myListUP);
-
-
-            })
-          })
-        });
-  }
-  //funcion de calculo de progreso
-
+  btn2.addEventListener('click', () => {
+    const render = cohorts.forEach(element => {
+      // element.name ==
+      let myCohorts = element.id;
+      let myList = document.createElement('ul');
+      let listItem = document.createElement('li');
+      let aItem = document.createElement('button');
+      aItem.textContent = myCohorts;
+      myList.appendChild(aItem);
+      container.appendChild(myList);
+      aItem.addEventListener('click', () => {
+        let usersStats = window.computeUsersStats(users, progress, Object.keys(element.coursesIndex));
+        let myListUP = document.createElement('ul');
+        let listItemUP = document.createElement('li');
+        let aItemUP = document.createElement('button');
+        aItemUP.textContent = usersStats.sort(); // Uso de sort para tener nombres por orden alfabético
+        myListUP.appendChild(aItemUP);
+        students.appendChild(myListUP);
+      });
+    });
+  });
+};
+//  función de calculo de progreso
 
 window.computeUsersStats = (users, progress, courses) => {
+  return users.map((user) => {
+    const userProgress = progress[user.id];
+    const users = user.name;
+    let percentSum = 0;
+    let totalCourses = 0;
+    for (let courseIndex in userProgress)
+    {
+      totalCourses++;
+      percentSum += userProgress[courseIndex].percent;
+    }
+    console.log('User > ' + JSON.stringify(user));
+    user.stats = {
+      percent: percentSum / totalCourses
+    };
 
-	return users.map((user)=>{
-		const userProgress = progress[user.id];
-		const users = user.name;
-		let percentSum = 0;
-		let totalCourses = 0;
-		for(let courseIndex in userProgress){
-			totalCourses++;
-			percentSum += userProgress[courseIndex].percent;
-		}
-		console.log("User > "+JSON.stringify(user));
-		user.stats = {
-			percent : percentSum / totalCourses 
-		};
-		
-		return JSON.stringify(user.name);
-	});
+    return JSON.stringify(user.name);
+  });
 };
-
-
 
 /*
 
