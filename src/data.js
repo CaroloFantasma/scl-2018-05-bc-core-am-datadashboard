be5371ddc48a8042a1dcbefc232dad4feb610732
Carolina Lastra <carolastra.cofre@gmail.com>
2018-07-24 19:43:30 -0400

    ahora también se imprime el progreso

--- src/data.js
+++ src/data.js
@@ -1,32 +1,24 @@
-const btn2 = document.getElementById('btn2');
-//  const myInput = document.getElementById('myInput');
-
-myInput.addEventListener('onkeyup', function() {
-  filterUsers();
-});
-
-//  Constantes de los id donde va el cohort y las alumnas
-
 const container = document.getElementById('cohorts');
-const students = document.getElementById('texto')
+const students = document.getElementById('texto');
 
 //  Variables de la data
 const usersJSON = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
 const progressJSON = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
 const cohortsJSON = '../data/cohorts.json';
-let users = null;
 
 //  Variables globales
+let users = null;
 let progress = null;
 let cohorts = null;
 let usersStats = null;
 
-//  Código de fetch para user, progress y cohort
+// Código de fetch para cohort, progress y users.
 // Fetch de la data de cohort
 fetch(cohortsJSON)
   .then(response => response.json())
   .then(data => {
     cohorts = data;
+    gettingData();
 
     console.log(data);
     renderCohorts(data);
@@ -39,11 +31,11 @@ fetch(progressJSON)
   .then(response => response.json())
   .then(data => {
     progress = data;
+    gettingData();
 
     console.log(data);
     renderCohorts(data);
     if (users && progress && cohorts) {
-
     }
   });
 
@@ -53,19 +45,19 @@ fetch(usersJSON)
   .then(response => response.json())
   .then(data => {
     users = data;
+    gettingData();
 
     console.log(data);
     renderCohorts(data);
     if (users && progress && cohorts) {
-      return
     }
   });
 
-//  Constante de declaracion de cohorts
+// Constante de declaración de cohorts
 
 const renderCohorts = cohorts => {
   btn2.addEventListener('click', () => {
-    const render = cohorts.forEach(element => {
+    const cohortList = cohorts.forEach(element => {
       // element.name ==
       let myCohorts = element.id;
       let myList = document.createElement('ul');
@@ -74,89 +66,64 @@ const renderCohorts = cohorts => {
       aItem.textContent = myCohorts;
       myList.appendChild(aItem);
       container.appendChild(myList);
-      aItem.addEventListener('click', () => {
-        let usersStats = window.computeUsersStats(users, progress, Object.keys(element.coursesIndex));
-        let myListUP = document.createElement('ul');
-        let listItemUP = document.createElement('li');
-        let aItemUP = document.createElement('button');
-        aItemUP.textContent = usersStats.sort(); // Uso de sort para tener nombres por orden alfabético
-        myListUP.appendChild(aItemUP);
-        students.appendChild(myListUP);
-      });
     });
   });
 };
-//  función de calculo de progreso
 
-window.computeUsersStats = (users, progress, courses) => {
-  return users.map((user) => {
-    const userProgress = progress[user.id];
-    const users = user.name;
-    let percentSum = 0;
-    let totalCourses = 0;
-    for (let courseIndex in userProgress)
-    {
-      totalCourses++;
-      percentSum += userProgress[courseIndex].percent;
-    }
-    console.log('User > ' + JSON.stringify(user));
-    user.stats = {
-      percent: percentSum / totalCourses
-    };
 
-    return JSON.stringify(user.name);
-  });
-};
-
-/*
-
-//window.sortUsers(users,"name", "ASC")
-window.sortUsers = (users, orderBy, orderDirection) => {
-  if (orderBy === 'name') {
-    return users.sort(function(nameList, nameOrder) {
-      if (orderDirection === 'ASC')
-        return nameList.name.localeCompare(nameOrder.name);
-      else
-        return nameList.name.localeCompare(nameOrder.name) * -1;
-    });
+function gettingData() { 
+  if (users && progress && cohorts) {
+    const cohort = cohorts.find(item => item.id === 'lim-2018-03-pre-core-pw');
+    const courses = Object.keys(cohort.coursesIndex);
+    usersStats = window.computeUsersStats(users, progress, courses); 
   }
-};
-
- esto no lo borres esta apagado porque todavia no funciona
-window.filterUsers = (users, search) => {
-	let input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
-    filter = input.value.toLowerCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsById("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsById("filter")[0];
-        if (a.innerHTML.toLowerCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-
-        }
-    }
+}
+function orderedList() {
+  const direction = listSort.innerText;
+  if (direction === 'ASC') {
+    listSort.innerText = 'DESC';
+  } else {
+    listSort.innerText = 'ASC';
+  }
+  const sortedUsers = window.sortUsers(usersStats, 'percent', direction);
+  studentContainer.innerHTML = '';
+  for (let student of sortedUsers) {
+    studentContainer.innerHTML += `
+        <p>${student.name} ${student.stats.percent}</p>
+      `;
+  }
+}
+  
+function filterInput() {
+  const search = searchInput.value;
+  const filteredUsers = window.filterUsers(usersStats, search);
+  studentContainer.innerHTML = '';
+  filteredUsers.forEach(student => {
+    studentContainer.innerHTML += `
+        <p>${student.name}</p>
+      `;
+  });
 }
 
+// //  función de cálculo de progreso
+
+// window.computeUsersStats = (users, progress, courses) => {
+//   return users.map((user) => {
+//     const userProgress = progress[user.id];
+//     const users = user.name;
+//     let percentSum = 0;
+//     let totalCourses = 0;
+//     for (let courseIndex in userProgress) {
+//       totalCourses++;
+//       percentSum += userProgress[courseIndex].percent;
+//     }
+//     console.log('User > ' + JSON.stringify(user));
+//     user.stats = {
+//       percent: percentSum / totalCourses
+//     };
+
+//     return JSON.stringify(user.name);
+//   });
+// };
 
 
-
-window.processCohortData = (options) => {
-	renderCohorts();
-
-};
-window.computeUsersStats = (users, progress, courses) => {
-
-
-}
-
-window.filterUsers = (users, search) => {
-	
-
-}
-
-window.processCohortData = (options) => {
-} 
-*/
--- src/main.js
+++ src/main.js
@@ -1,1 +1,137 @@
+window.computeUsersStats = (
+  users, 
+  progress, 
+  courses 
+) => {
+  for (let i = 0; i < users.length; i++) {
+    let userId = users[i].id;
+    
+    let userProgress = progress[userId];
+    if (JSON.stringify(userProgress) === '{}') {
+      users[i].stats = {
+        percent: 0,
+        exercises: { percent: 0, },
+        reads: { percent: 0, },
+        quizzes: {
+          percent: 0,
+          scoreAvg: 0,
+        }
+      };
+    } else {
+      let percentGral = 0;
+      let lectures = 0;
+      let lecturesCompleted = 0;
+      let lecturesPercent = 0;
+      let quizzes = 0;
+      let quizzesCompleted = 0;
+      let quizzesPercent = 0;
+      let exercises = 0;
+      let exercisesCompleted = 0;
+      let exercisesPercent = 0;
+      let scoreSum = 0;
+      let scoreAvg = 0;
 
+      
+      for (let i in userProgress) { 
+        let element = userProgress[i];
+        if (courses.indexOf(i) < 0) {
+          continue;
+        }
+        
+        percentGral += element.percent / Object.keys(userProgress).length;
+        for (let unit of Object.values(element.units)) { 
+          for (let part of Object.values(unit.parts)) { 
+            if (part.length === 0) {
+              quizzes = 0;
+              exercises = 0;
+              lectures = 0;
+              quizzesPercent = 0;
+              exercisesPercent = 0;
+              lecturesPercent = 0;
+            }
+            if (part.type === 'read') {
+              lectures++;
+            }
+            if (part.type === 'read' && part.completed === 1) {
+              lecturesCompleted++;
+            }
+            lecturesPercent = Math.round((lecturesCompleted * 100) / lectures);
+            if (part.type === 'quiz') { 
+              quizzes++;
+            }
+            if (part.type === 'quiz' && part.completed === 1) {
+              quizzesCompleted++;
+              scoreSum += part.score;
+            }
+            quizzesPercent = Math.round((quizzesCompleted * 100 * 10 / quizzes)) / 10;// truco para sacar 1 decimal
+            if (part.type === 'practice') {
+              exercises++;
+            }
+            if (part.type === 'practice' && part.completed === 1) {
+              exercisesCompleted++;
+            }
+            exercisesPercent = Math.round((exercisesCompleted * 100 * 10) / (exercises || 1)) / 10; 
+          } 
+        } 
+      }
+      scoreAvg = scoreSum / quizzes;
+
+      users[i].stats = {
+        percent: percentGral,
+        reads: {
+          percent: lecturesPercent,
+          total: lectures,
+          completed: lecturesCompleted
+        },
+        exercises: {
+          percent: exercisesPercent,
+          total: exercises,
+          completed: exercisesCompleted
+        },
+        quizzes: {
+          percent: quizzesPercent,
+          total: quizzes,
+          completed: quizzesCompleted,
+          scoreAvg: scoreAvg,
+          scoreSum: scoreSum
+        }
+      };
+    }
+  }
+  
+  return users;
+};
+
+window.sortUsers = (users, orderBy, orderDirection) => { 
+  if (orderBy === 'name') { 
+    return users.sort(function(ab, cd) {
+      if (orderDirection === 'ASC') {
+        return ab.name.localeCompare(cd.name);
+      } else {
+        return ab.name.localeCompare(cd.name) * -1;
+      }
+    });
+  }
+
+  if (orderBy === 'percent') {
+    return users.sort((ab, cd)=> {
+      if (orderDirection === 'ASC') {
+        return ab.stats.percent - cd.stats.percent;
+      } else {
+        return (ab.stats.percent - cd.stats.percent) * -1;
+      }
+    });
+  }
+};
+
+window.filterUsers = (users, search) => {
+  if (search) {
+    if (users) {
+      search = search.toLowerCase();
+      return users.filter(user => user &&
+         user.name &&
+        user.name.toLowerCase().indexOf(search) >= 0);
+    }
+  }
+  return users;
+};
